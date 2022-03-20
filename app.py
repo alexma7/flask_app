53869bcd23707357d685cc395324a49ff38672a9
Alex <alexma7@rambler.ru>
2022-03-21 01:51:26 +0300

    Неделя 1

--- app.py
+++ app.py
@@ -1,22 +1,55 @@
-from flask import Flask#, request, render_template, jsonify
+import flask #, request, render_template, jsonify
 #from flask.wrappers import Response
 #from py.prime import makePrime
 #from galton import galtonboard
-import git # GitPython library
+#import git # GitPython library
 #import os
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 #Route for the GitHub webhook
-@app.route('/git_update', methods=['POST'])
-def git_update():
-  repo = git.Repo('./flask_app')
-  origin = repo.remotes.origin
-  repo.create_head('main', 
-  origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
-  origin.pull()
-  return '', 200
+# @app.route('/git_update', methods=['POST'])
+# def git_update():
+#   repo = git.Repo('./flask_app')
+#   origin = repo.remotes.origin
+#   repo.create_head('main', 
+#   origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+#   origin.pull()
+#   return '', 200
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!888</p>"
+    return flask.render_template('index.html')
+
+@app.route("/about")
+def about():
+    return flask.render_template('about.html')
+
+@app.route("/welcome")
+def welcome():
+    weeks = 5
+    course = 'Flask'
+    group = 'ПО-09'
+    return flask.render_template(
+        'welcome.html', 
+        weeks=weeks,  
+        course=course, 
+        group=group
+    )
+
+@app.route('/students/<int:student_id>')
+def students(student_id):
+    students = {
+      1: 'Петров Иван Иванович',
+      2: 'Иванов Иван Иванович',
+      3: 'Сидоров Иван Иванович',
+      4: 'Кукухин Иван Иванович',
+    }
+    student_name = students.get(student_id)
+    if student_name is None:
+        flask.abort(404)
+    return flask.render_template('students.html', student_name=student_name)
+
+if __name__ == '__main__':
+  app.run(debug=True)
+ 
