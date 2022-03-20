939be906e4aaf0e51eec42191ddf01217dcaf5e9
alexma7 <40296450+alexma7@users.noreply.github.com>
2022-03-20 14:25:39 +0300

    Update app.py
    Обновление по вебхуку

--- app.py
+++ app.py
@@ -2,6 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+#Route for the GitHub webhook
+@app.route('/git_update', methods=['POST'])
+def git_update():
+  repo = git.Repo('./orbe')
+  origin = repo.remotes.origin
+  repo.create_head('main', 
+  origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+  origin.pull()
+  return '', 200
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!1111</p>"
